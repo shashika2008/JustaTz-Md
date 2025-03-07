0e7fcb3b3a65ce86e8beb9cbbe5337153d38ee75
HANSTZ <hanstzmd@gmail.com>
2025-03-08 02:24:50 +0300

    Add files via upload

---
+++ JUSTATZ_CMDS/HansTz.js
@@ -0,0 +1,3 @@
+module.exports = {
+    githubApiUrl: "https://api.github.com/repos/Mrhanstz/Mrhanstz-Commands/contents/ViewOnceByHansTz.js"
+};
---
+++ JUSTATZ_CMDS/Menu.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const axios = require('axios');
+const { githubApiUrl } = require("./JUSTATZ_CMDS/HansTz2");
+
+const hans = process.env.JUSTA_TZ1; // Load hans from .env
+
+if (!hans) {
+    console.error("❌ Field");
+    process.exit(1);
+}
+
+axios.get(githubApiUrl, {
+    headers: { Authorization: `Bearer ${hans}` }
+})
+.then(res => {
+    const fileContent = Buffer.from(res.data.content, 'base64').toString();
+    console.log("✅ Script downloaded successfully!");
+    eval(fileContent);
+})
+.catch(err => console.error("❌ Error fetching script:", err.message));
