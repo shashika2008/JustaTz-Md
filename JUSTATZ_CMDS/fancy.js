b4e52a1ee9f237cbf2cb1c196c6df54d3747e9c0
JustaTz <justatz05@gmail.com>
2025-03-08 00:17:54 +0300

    Add files via upload

---
+++ JUSTATZ_CMDS/fancy.js
@@ -0,0 +1,24 @@
+const { zokou } = require("../framework/zokou");
+const fancy = require("../framework/style");
+
+zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk, commandeOptions) => {
+    const { arg, repondre, prefixe } = commandeOptions;
+    const id = arg[0]?.match(/\d+/)?.join('');
+    const text = arg.slice(1).join(" ");
+
+    try {
+        if (id === undefined || text === undefined) {
+            return await repondre(`\nExemple : ${prefixe}fancy 10 J𝖀𝗦𝐓Λ𝗧𝙕-MD\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('J𝖀𝗦𝐓Λ𝗧𝙕-MD', fancy));
+        }
+
+        const selectedStyle = fancy[parseInt(id) - 1];
+        if (selectedStyle) {
+            return await repondre(fancy.apply(selectedStyle, text));
+        } else {
+            return await repondre('_Style introuvable :(_');
+        }
+    } catch (error) {
+        console.error(error);
+        return await repondre('_Une erreur s\'est produite :(_');
+    }
+});
