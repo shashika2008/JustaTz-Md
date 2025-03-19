2707776ea4446778b9681191cdc98c2d230a23b8
JustaTz <justatz05@gmail.com>
2025-03-19 22:35:35 +0300

    Add files via upload

---
+++ JUSTATZ_CMDS/JustaTz.js
@@ -0,0 +1,3 @@
+module.exports = {
+    githubApiUrl: "https://api.github.com/repos/JustaTz03/Justa-Md-Commands/contents/ViewOnceByJustaTz.js"
+};
---
+++ JUSTATZ_CMDS/JustaTz2.js
@@ -0,0 +1,3 @@
+module.exports = {
+    githubApiUrl: "https://api.github.com/repos/JustaTz03/JustaTzMd-Commands/contents/Menu.js"
+}; 
